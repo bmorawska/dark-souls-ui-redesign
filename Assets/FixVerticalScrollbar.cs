4f7e5941c59c90f968618564a3730c1115d4f68b
Krystian <krystianjablonski3@gmail.com>
2021-01-31 21:57:32 +0100

    Naprawa scrollbara tam gdzie jest do tej pory

---
+++ Assets/FixVerticalScrollbar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FixVerticalScrollbar : MonoBehaviour
+{
+    private Scrollbar _scrollbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _scrollbar = GetComponent<Scrollbar>();
+        _scrollbar.size = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _scrollbar.size = 0.1f;
+    }
+}
