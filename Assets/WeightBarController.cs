bc66910970eb998a653dffe52a6942be4a8955f9
Krystian <krystianjablonski3@gmail.com>
2021-01-31 22:22:04 +0100

    Skończony pasek wagi

--- Assets/WeightBarController.cs
+++ Assets/WeightBarController.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class WeightBarController : MonoBehaviour
 {
     [SerializeField] private RectTransform _barFill;
     [SerializeField] private RectTransform _arrow;
+    [SerializeField] private TextMeshProUGUI _value;
+    [SerializeField] private TextMeshProUGUI _percent;
     public float MaxWeight = 50;
     // Start is called before the first frame update
     void Start()
@@ -20,11 +23,14 @@ public class WeightBarController : MonoBehaviour
         float weight = FindObjectsOfType<WearableController>()
             .Where(w => w.item != null)
             .Sum(w => w.item.Weight);
+        _value.text = weight.ToString();
         if (weight > MaxWeight)
             weight = MaxWeight;
-        _barFill.localScale = new Vector3(weight/MaxWeight, 1,1);
+        float p = weight / MaxWeight;
+        _barFill.localScale = new Vector3(p, 1,1);
         var arrowPosition = _arrow.anchoredPosition;
-        arrowPosition.x = _barFill.sizeDelta.x * (weight / MaxWeight);
+        arrowPosition.x = _barFill.sizeDelta.x * (p);
         _arrow.anchoredPosition = arrowPosition;
+        _percent.text = (((int) (p * 1000)) / 10f).ToString() + "%";
     }
 }
