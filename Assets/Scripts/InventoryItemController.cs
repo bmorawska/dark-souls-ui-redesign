293eac58f409ba5f960f38a776df01a7f297949f
Krystian <krystianjablonski3@gmail.com>
2021-01-31 15:13:57 +0100

    Tło itemów, jeszcze nie skończone

--- Assets/Scripts/InventoryItemController.cs
+++ Assets/Scripts/InventoryItemController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class InventoryItemController : MonoBehaviour
@@ -13,11 +14,17 @@ public class InventoryItemController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _itemNameObject;
     //[SerializeField] private TextMeshProUGUI _itemDescriptionObject;
     [SerializeField] private Image _itemIcon;
+    [SerializeField] private GameObject _countBackground;
+    [SerializeField] private TextMeshProUGUI _countText;
     public Item Item { get; set; }
 
     private void Start()
     {
         _itemNameObject.text = Item.Name;
+        if (Item.Count == 0 || Item.Count == 1)
+            _countBackground.SetActive(false);
+        else
+            _countText.text = Item.Count.ToString();
         //_itemDescriptionObject.text = Item.Description;
         _itemIcon.sprite = Item.icon;
         CategoryItemsAssigment assigment = GetComponent<CategoryItemsAssigment>();
@@ -27,7 +34,7 @@ public class InventoryItemController : MonoBehaviour
     
     private void OnEnable()
     {
-        ChangeChildrenTextColorDefault();
+        GetComponent<EventTrigger>().OnDeselect(null);
     }
 
     public void ChooseItem()
